ae1acf0301ae40f6eeb559194d5456186e512db1
M0HID <mohidrehman321@gmail.com>
2024-08-08 18:27:24 +0100

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,29 @@ import React, {useState} from 'react';
 
 function App() {
   const [placeholderText, setPlaceholderText] = useState('');
+  const [equation, setEquation] = useState('');
+  const [operatorPressed, setOperatorPressed] = useState(false);
 
-  // Step 2: Update state on button click
+
   const handleButtonClick = (value) => {
-    // setPlaceholderText((prev) => prev + value);
-    if(placeholderText.length < 10) {
+    if (operatorPressed) {
+      setPlaceholderText(value);
+      setOperatorPressed(false);
+    }
+    else if(placeholderText.length < 10) {
       if (placeholderText === '0' && value !== '.') {
         setPlaceholderText(value);
+        setEquation(value);
       }
       else {
         setPlaceholderText((prev) => prev + value);
+        setEquation((prev) => prev + value);
       }
     }
   };
   const handleClear = () => {
-    setPlaceholderText('');
-  }
-  const handlePlusMinus = () => {
-    const lastOperatorIndex = placeholderText.lastIndexOf('+') > placeholderText.lastIndexOf('-') ? placeholderText.lastIndexOf('+') : placeholderText.lastIndexOf('-');
-    console.log(lastOperatorIndex);
-    const lastNumber = placeholderText.substring(lastOperatorIndex + 1);
-    console.log(lastNumber);
-    const newNumber = parseFloat(lastNumber) * -1;
-    const newPlaceholderText = placeholderText.substring(0, lastOperatorIndex + 1) + newNumber;
-    setPlaceholderText(newPlaceholderText);
+    setPlaceholderText('0');
+    setEquation('0');
   }
   const handlePercent = () => {
     setPlaceholderText((prev) => {
@@ -51,6 +50,18 @@ function App() {
       }
       return prev;
     });
+    setEquation((prev) => {
+      if (!prev.includes('.')) {
+        if (prev === '') {
+          return '0.';
+        }
+        else {
+          return prev + '.';
+        }
+      }
+      return prev;
+    }
+    );
   }
   const handleZero = () => {
     setPlaceholderText((prev) => {
@@ -60,6 +71,37 @@ function App() {
       return prev;
     });
   }
+  const handlePlus = () => {
+    setEquation((prev) => {placeholderText + '+'});
+    console.log('Operator pressed is ', operatorPressed);
+    setOperatorPressed(true);
+    console.log('Operator pressed is ', operatorPressed);
+
+    console.log('Equation is ', equation);
+  }
+  const handleMinus = () => {
+    setEquation((prev) => {placeholderText + '-'});
+  }
+  const handleMultiply = () => {
+    setEquation((prev) => {placeholderText + '*'});
+  }
+  const handleDivide = () => {
+    setEquation((prev) => {placeholderText + '/'});
+  }
+  const handlePlusMinus = () => {
+    setPlaceholderText((prev) => {
+      if (prev !== '0') {
+        console.log('prev is ', prev);
+        return (parseFloat(prev) * -1).toString();
+      }
+      else {
+        console.log('Value of prev: ', prev);
+        return ('-0');
+      }
+    }
+    );
+  }
+  
   const handleEquals = () => {
     const replacedText = placeholderText.replace(/÷/g, '/').replace(/×/g, '*');
     try {
@@ -102,7 +144,7 @@ function App() {
         <button className="button-dark-gray" onClick={() => handleButtonClick('1')}>1</button>
         <button className="button-dark-gray" onClick={() => handleButtonClick('2')}>2</button>
         <button className="button-dark-gray" onClick={() => handleButtonClick('3')}>3</button>
-        <button className="button-orange" onClick={() => handleButtonClick('+')}>+</button>
+        <button className="button-orange" onClick={() => handlePlus()}>+</button>
       </div>
       <div className="flex">
         <button className="button-big" onClick={() => handleZero()}>0</button>
